d1b4cb3eea6f05beac5529eed0e82a18b51528f8
James Hovet <jrhovet@gmail.com>
2021-06-08 15:47:17 -0500

    remove dead code

--- src/pyext.py
+++ src/pyext.py
@@ -14,7 +14,6 @@ else:
 import traceback
 import struct
 
-
 LEN_SIZE = 10
 TYPE_SIZE = 1
 
@@ -28,85 +27,6 @@ EXPR_STRINGIFIED_MSG = 3
 
 SUCC_MSG = 0
 ERR_MSG = 1
-
-
-def print_err(s):
-    sys.stderr.write('{}\n'.format(s))
-    sys.stderr.flush()
-
-
-# class ConnectionReader(object):
-#     def __init__(self, conn):
-#         self.conn = conn
-#         self.buff = bytearray()
-#
-#     def _get_packet(self):
-#         data = self.conn.recv(1024)
-#         if not data:
-#             raise EOFError('Connection closed')
-#         return data
-#
-#     def read(self, size):
-#         while len(self.buff) < size:
-#             self.buff.extend(self._get_packet())
-#         result = bytes(self.buff[:size])
-#         del self.buff[:size]
-#         return result
-#
-#     def read_json(self):
-#         return json.loads(self.read_string())
-#
-#     def read_int(self):
-#         return struct.unpack('>i', self.read(4))[0]
-#
-#     def read_byte(self):
-#         return struct.unpack('b', self.read(1))[0]
-#
-#     def read_string(self):
-#         length = self.read_int()
-#         return self.read(length).decode('utf-8')
-
-
-# class ConnectionWriter(object):
-#     def __init__(self, conn):
-#         self.conn = conn
-#         self.buff = bytearray()
-#
-#     def write(self, data):
-#         self.buff.extend(data)
-#
-#     def write_byte(self, b):
-#         self.write(struct.pack('b', b))
-#
-#     def write_int(self, i):
-#         self.write(struct.pack('>i', i))
-#
-#     def write_string(self, s):
-#         bs = to_bytes(s)
-#         self.write_int(len(bs))
-#         self.write(bs)
-#
-#     def flush(self):
-#         self.conn.sendall(self.buff)
-#         self.clear()
-#
-#     def clear(self):
-#         self.buff = bytearray()
-
-
-def utf8(bs):
-    if sys.version_info >= (3, 0):
-        return str(bs, 'UTF8')
-    else:
-        return unicode(bs, 'UTF8')
-
-
-def to_bytes(s):
-    if sys.version_info >= (3, 0):
-        return bytes(s, 'UTF8')
-    else:
-        return bytes(s)
-
 
 class FlexibleEncoder(json.JSONEncoder):
     def default(self, o):
@@ -125,9 +45,6 @@ class FlexibleEncoder(json.JSONEncoder):
 def logo_responder():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        # p = 1337
-        # p = 1337 if 'manual' in sys.argv else 1336
-        # sock.bind(('localhost', p))
         sock.bind(('localhost', 0))
         sock.listen(0)
         _, port = sock.getsockname()
@@ -135,36 +52,26 @@ def logo_responder():
         sys.stdout.flush()
         conn, addr = sock.accept()
         try:
-            # inp = ConnectionReader(conn)
-            # out = ConnectionWriter(conn)
             encoder = FlexibleEncoder()
             globs = {}
-            # print("got connection")
             for line in conn.makefile():
                 try:
-                    # print("new line:", line)
                     decoded = json.loads(line)
-                    # print("new data:", decoded)
-
                     type = decoded["type"]
                     body = decoded["body"]
 
                     if type == STMT_MSG:
                         exec(body, globs)
                         conn.sendall(json.dumps({"type" : SUCC_MSG, "body" : ""}).encode('utf-8') + b"\n")
-                        # out.write_byte(SUCC_MSG)
                     elif type == EXPR_MSG:
                         evaluated = eval(body, globs)
                         encoded = encoder.encode({"type" : SUCC_MSG, "body" : evaluated})
                         conn.sendall(encoded.encode('utf-8') + b"\n")
-                        # out.write_byte(SUCC_MSG)
-                        # out.write_string(res)
                     elif type == ASSN_MSG:
                         varName = body["varName"]
                         value = body["value"]
                         globs[varName] = value
                         conn.sendall(json.dumps({"type" : SUCC_MSG, "body" : ""}).encode('utf-8') + b"\n")
-                        # out.write_byte(SUCC_MSG)
                     elif type == EXPR_STRINGIFIED_MSG:
                         evaluated = repr(eval(body, globs))
                         encoded = encoder.encode({"type" : SUCC_MSG, "body" : evaluated})
@@ -172,40 +79,8 @@ def logo_responder():
                 except Exception as e:
                     err_data = {"type" : ERR_MSG, "body" : {"message" : str(e), "cause" : traceback.format_exc()}}
                     conn.sendall(encoder.encode(err_data).encode('utf-8') + b"\n")
-                    # out.write_byte(ERR_MSG)
-                    # out.write_string(str(e))
-                    # out.write_string(traceback.format_exc())
                 finally:
-                    # out.flush()
                     flush()
-
-            # while True:
-            #     # msg_type = inp.read_byte()
-            #     try:
-            #         print("line", conn.makefile().readline())
-            #         # if msg_type == STMT_MSG:
-            #         #     code = inp.read_string()
-            #         #     exec(code, globs)
-            #         #     out.write_byte(SUCC_MSG)
-            #         # elif msg_type == EXPR_MSG:
-            #         #     code = inp.read_string()
-            #         #     result = encoder.encode(eval(code, globs))
-            #         #     out.write_byte(SUCC_MSG)
-            #         #     out.write_string(result)
-            #         # elif msg_type == ASSN_MSG:
-            #         #     var = inp.read_string()
-            #         #     val = json.loads(inp.read_string())
-            #         #     globs[var] = val
-            #         #     out.write_byte(SUCC_MSG)
-            #         # else:
-            #         #     raise Exception('Unrecognized message type: {}'.format(msg_type))
-            #     except Exception as e:
-            #         out.write_byte(ERR_MSG)
-            #         out.write_string(str(e))
-            #         out.write_string(traceback.format_exc())
-            #     finally:
-            #         out.flush()
-            #         flush()
         finally:
             conn.close()
     finally:
